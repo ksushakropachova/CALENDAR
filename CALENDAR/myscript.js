5ed8d4f33084086063e3d257922357de3d4de2f6
CodeNeBo <nechitaylob@gmail.com>
2022-08-06 17:50:01 +0300

    Added calendar script

--- CALENDAR/myscript.js
+++ CALENDAR/myscript.js
@@ -2,6 +2,10 @@ let date = new Date();
 let day = date.getDate();
 let year = date.getFullYear();
 let month = date.getMonth();
+let monthDays = document.querySelector(".calendar_days");
+let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); 
+
+date.setDate(1);
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"];
@@ -12,8 +16,38 @@ calendarTitle.append(monthNames[month]  + " " + year);
 let currentdate = document.querySelector(".currentdate" );
 currentdate.append( day + " "+ monthNames[month] + " "+ year);
 
-const firstDateOfMonth = new Date(date);
-firstDateOfMonth.setDate(1);
+let days = "";
+
+for (let i = 1; i <= lastDay; i++){
+  days += `<div>${i}</div>`;
+  monthDays.innerHTML = days;
+};
+
+console.log(lastDay)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
